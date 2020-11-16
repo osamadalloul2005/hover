10e634c5a16da11b4f3a1e701f86ae204594fb53
osama dalloul <58133188+osamadalloul2005@users.noreply.github.com>
2020-11-16 23:55:08 +0200

    hover

---
+++ js/plugins.js
@@ -0,0 +1,23 @@
+//document.write("hi osama");
+
+
+//document.getElementById("result").innerHTML="Javascript";
+//
+//document.getElementById("data").innerHTML="Hi java";
+
+
+document.getElementById("btn-result").onclick=function(){
+	
+	document.getElementById("result").innerHTML="Hi Java";
+	
+};
+
+document.getElementById("btn-data").onclick=function(){
+	
+	document.getElementById("data").innerHTML="Hi JavaScript";
+	
+};
+
+var x="osama";
+
+console.log(x);
